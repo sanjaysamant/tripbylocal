676f4f425ddcd243c50be148acc90d3c16608fca
samant <sanjaysamant.gpr@gmail.com>
2018-12-26 20:00:30 +0530

    hit and try to fix the path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express'),
 path = require('path');
 
 const app = express();
-app.use(express.static('.dist/'));
+app.use(express.static(path.join(__dirname, '/dist')));
 
 app.get('/', (req, res) => {
 
