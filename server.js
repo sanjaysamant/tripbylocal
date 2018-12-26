f56c59fa084423e2b61f8df0fcdc7674c2eb491b
samant <sanjaysamant.gpr@gmail.com>
2018-12-26 19:36:57 +0530

    changes in dist path

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express'),
 path = require('path');
 
 const app = express();
-app.use(express.static('.dist/material-dashboard-angular'));
+app.use(express.static('.dist/'));
 
 app.get('/', (req, res) => {
 
-    res.sendFile(path.join(__dirname, 'dist/material-dashboard-angular/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, () => {
